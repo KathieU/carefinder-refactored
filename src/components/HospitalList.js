b63b05bc056e76487235e6b7002cd51ade8dcdd3
Catherine <kathrynepatrick@gmail.com>
2023-06-10 02:26:31 +0100

    implemented the search and export functionalities

--- src/components/HospitalList.js
+++ src/components/HospitalList.js
@@ -1,6 +1,58 @@
+// import React, { useEffect, useState } from "react";
+// import { useParams } from "react-router-dom";
+// import { firestore } from "../firebase";
+
+// function HospitalList() {
+//   const [hospitals, setHospitals] = useState([]);
+//   const { id } = useParams();
+
+//   useEffect(() => {
+//     const fetchHospitals = async () => {
+//       try {
+//         const hospitalsSnapshot = await firestore
+//           .collection("locations")
+//           .doc(id)
+//           .collection("hospitals")
+//           .get();
+
+//         const hospitalsData = hospitalsSnapshot.docs.map((doc) => ({
+//           id: doc.id,
+//           ...doc.data(),
+//         }));
+
+//         setHospitals(hospitalsData);
+//       } catch (error) {
+//         console.error("Error fetching hospitals:", error);
+//       }
+//     };
+
+//     fetchHospitals();
+//   }, [id]);
+
+//   return (
+//     <div>
+//       <h1>Hospitals in Location</h1>
+//       <ul>
+//         {hospitals.map((hospital) => (
+//           <li key={hospital.id}>
+//             <strong>{hospital.name}</strong>
+//             <br />
+//             Location: {hospital.location}
+//             <br />
+//             Address: {hospital.address}
+//           </li>
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// }
+
+// export default HospitalList;
+
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { firestore } from "../firebase";
+import { CSVLink } from "react-csv";
 
 function HospitalList() {
   const [hospitals, setHospitals] = useState([]);
@@ -29,6 +81,18 @@ function HospitalList() {
     fetchHospitals();
   }, [id]);
 
+  const csvHeaders = [
+    { label: "Name", key: "name" },
+    { label: "Location", key: "location" },
+    { label: "Address", key: "address" },
+  ];
+
+  const csvData = hospitals.map((hospital) => ({
+    name: hospital.name,
+    location: hospital.location,
+    address: hospital.address,
+  }));
+
   return (
     <div>
       <h1>Hospitals in Location</h1>
@@ -43,78 +107,11 @@ function HospitalList() {
           </li>
         ))}
       </ul>
+      <CSVLink data={csvData} headers={csvHeaders}>
+        Export to CSV
+      </CSVLink>
     </div>
   );
 }
 
 export default HospitalList;
-
-//   const handleLocationClick = (location) => {
-//     setSelectedLocation(location);
-//   };
-
-//   useEffect(() => {
-//     const fetchHospitals = async () => {
-//       if (selectedLocation) {
-//         try {
-//           const hospitalsSnapshot = await firestore
-//             .collection("locations")
-//             .doc(selectedLocation.id)
-//             .collection("hospitals")
-//             .get();
-
-//           const hospitalsData = hospitalsSnapshot.docs.map((doc) => ({
-//             id: doc.id,
-//             locationId: selectedLocation.id,
-//             locationName: selectedLocation.name,
-//             ...doc.data(),
-//           }));
-
-//           setHospitals(hospitalsData);
-//         } catch (error) {
-//           console.error("Error fetching hospitals:", error);
-//         }
-//       }
-//     };
-
-//     fetchHospitals();
-//   }, [selectedLocation]);
-
-//   const [hospitals, setHospitals] = useState([]);
-
-//   return (
-//     <div>
-//       <h1>List of Locations</h1>
-//       <ul>
-//         {locations.map((location) => (
-//           <li
-//             key={location.id}
-//             onClick={() => handleLocationClick(location)}
-//             style={{ cursor: "pointer" }}
-//           >
-//             {location.name}
-//           </li>
-//         ))}
-//       </ul>
-
-//       {selectedLocation && (
-//         <div>
-//           <h2>Hospitals in {selectedLocation.name}</h2>
-//           <ul>
-//             {hospitals.map((hospital) => (
-//               <li key={hospital.id}>
-//                 <strong>{hospital.name}</strong>
-//                 <br />
-//                 Location: {hospital.locationName}
-//                 <br />
-//                 Address: {hospital.address}
-//               </li>
-//             ))}
-//           </ul>
-//         </div>
-//       )}
-//     </div>
-//   );
-// }
-
-// export default HospitalList;
--- src/components/LocationList.js
+++ src/components/LocationList.js
@@ -1,14 +1,58 @@
+// import React, { useEffect, useState } from "react";
+// import { Link } from "react-router-dom";
+// import { firestore } from "../firebase";
+
+// function LocationList() {
+//   const [locations, setLocations] = useState([]);
+
+//   useEffect(() => {
+//     const fetchLocations = async () => {
+//       try {
+//         const locationsSnapshot = await firestore.collection("locations").get();
+//         const locationsData = locationsSnapshot.docs.map((doc) => ({
+//           id: doc.id,
+//           ...doc.data(),
+//         }));
+//         setLocations(locationsData);
+//       } catch (error) {
+//         console.error("Error fetching locations:", error);
+//       }
+//     };
+
+//     fetchLocations();
+//   }, []);
+
+//   return (
+//     <div>
+//       <h1>List of Locations</h1>
+//       <ul>
+//         {locations.map((location) => (
+//           <li key={location.id}>
+//             <Link to={`/location/${location.id}`}>{location.name}</Link>
+//           </li>
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// }
+
+// export default LocationList;
+
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { firestore } from "../firebase";
 
 function LocationList() {
   const [locations, setLocations] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
 
   useEffect(() => {
     const fetchLocations = async () => {
       try {
-        const locationsSnapshot = await firestore.collection("locations").get();
+        const locationsSnapshot = await firestore
+          .collection("locations")
+          .where("name", ">=", searchTerm)
+          .get();
         const locationsData = locationsSnapshot.docs.map((doc) => ({
           id: doc.id,
           ...doc.data(),
@@ -20,11 +64,21 @@ function LocationList() {
     };
 
     fetchLocations();
-  }, []);
+  }, [searchTerm]);
+
+  const handleSearchChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
 
   return (
     <div>
       <h1>List of Locations</h1>
+      <input
+        type="text"
+        value={searchTerm}
+        onChange={handleSearchChange}
+        placeholder="Search by location..."
+      />
       <ul>
         {locations.map((location) => (
           <li key={location.id}>
